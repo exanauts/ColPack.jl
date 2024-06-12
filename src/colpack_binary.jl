4b6464616fe4e59c83a06f2892e8082bbea08fc6
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-06-12 00:44:13 -0400

    Apply suggestions from code review
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- src/colpack_binary.jl
+++ src/colpack_binary.jl
@@ -1,17 +1,18 @@
 """
 	colpack(file::String, method::String, order::String; verbose::Bool=false)
 
-Perform graph coloring.
+Perform graph coloring with the [ColPack executable](https://github.com/CSCsw/ColPack?tab=readme-ov-file#usage).
 
 - `file`: Indicates the graph file path.
 - `method`: Indicates the method.
 - `order`: Indicates the ordering.
 - `verbose`: Indicates verbose flag will be turned on and there will display more rich information.
 
-Examples:
-colpack("./bcsstk01.mtx", "DISTANCE_ONE", "RANDOM", verbose=true)
-colpack("./bcsstk01.mtx", "ROW_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
-colpack("./bcsstk01.mtx", "COLUMN_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
+# Examples
+
+    colpack("./bcsstk01.mtx", "DISTANCE_ONE", "RANDOM", verbose=true)
+    colpack("./bcsstk01.mtx", "ROW_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
+    colpack("./bcsstk01.mtx", "COLUMN_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
 """
 function colpack(file::String, method::String, order::String; verbose::Bool=false)
 	if verbose
