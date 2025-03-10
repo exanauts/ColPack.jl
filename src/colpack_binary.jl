7a7747b07e67813f8b673570667af17d84f9340d
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-03-10 20:01:06 +0100

    Add nthreads option

--- src/colpack_binary.jl
+++ src/colpack_binary.jl
@@ -1,5 +1,5 @@
 """
-	colpack(file::String, method::String, order::String; verbose::Bool=false)
+	colpack(file::String, method::String, order::String; verbose::Bool=false, threading::Bool=false)
 
 Perform graph coloring with the [ColPack executable](https://github.com/CSCsw/ColPack?tab=readme-ov-file#usage).
 
@@ -7,17 +7,20 @@ Perform graph coloring with the [ColPack executable](https://github.com/CSCsw/Co
 - `method`: Indicates the method.
 - `order`: Indicates the ordering.
 - `verbose`: Indicates verbose flag will be turned on and there will display more rich information.
+- `nthreads`: Indicates the number of threads (defaults to 1)
 
 # Examples
 
     colpack("./bcsstk01.mtx", "DISTANCE_ONE", "RANDOM", verbose=true)
     colpack("./bcsstk01.mtx", "ROW_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
-    colpack("./bcsstk01.mtx", "COLUMN_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true)
+    colpack("./bcsstk01.mtx", "COLUMN_PARTIAL_DISTANCE_TWO", "NATURAL", verbose=true, nthreads::Int=1)
 """
-function colpack(file::String, method::String, order::String; verbose::Bool=false)
-	if verbose
-		run(`$(ColPack_jll.ColPack()) -f $file -m $method -o $order -v`)
-	else
-		run(`$(ColPack_jll.ColPack()) -f $file -m $method -o $order`)
-	end
+function colpack(
+    file::String, method::String, order::String; verbose::Bool=false, nthreads::Int=1
+)
+    if verbose
+        run(`$(ColPack_jll.ColPack()) -f $file -m $method -o $order -nT $nthreads -v`)
+    else
+        run(`$(ColPack_jll.ColPack()) -f $file -m $method -o $order -nT $nthreads`)
+    end
 end
